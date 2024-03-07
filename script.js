bc17bf2da885c2716840004d843b3753f1617ef7
HudsonWayne <waynewaynebenhura@gmail.com>
2024-03-07 14:15:17 +0200

    the first lesson Javascript with Alison

---
+++ script.js
@@ -0,0 +1,13 @@
+let firstName = "Wayne\"Hi\"";
+let lastName = 'Benhura\'Hello\'';
+let fullName = "Wayne Benhura";
+const id = '100';
+// console.log(id);
+firstName = "new name";
+
+console.log("firstName");
+console.log("lastName");
+console.log("fullName");
+//document.write("Hello World!");
+console.log("Hello");
+// alert("Hello")

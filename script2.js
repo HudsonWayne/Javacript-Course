1b2ebcd8ddcfb191ba004d184a956a3b9c8ad488
HudsonWayne <waynewaynebenhura@gmail.com>
2024-03-07 15:32:57 +0200

    wayne

---
+++ script2.js
@@ -0,0 +1,19 @@
+
+//STRINGS
+
+let num = 100;
+let num1 = '100';
+
+num = num + 100;
+num1 = num1 + 100;
+
+// console.log(typeof(num));
+// console.log(typeof(num1));
+
+//BOOLEAN DATA TYPE IT TAKES TRUE OR FALSE
+ 
+let boo = true;
+console.log(boo);
+
+let arr =['Hallo',1,2,3,'wayne',true,[1,2,3]];
+console.log(typeof(arr))

b7df9847b9fe23e7e57e05fc9dd48f7c986ffc52
HudsonWayne <waynewaynebenhura@gmail.com>
2024-03-11 11:47:20 +0200

    comments

--- script2.js
+++ script2.js
@@ -1,19 +1,63 @@
 
-//STRINGS
+// //STRINGS
 
-let num = 100;
-let num1 = '100';
+// let num = 100;
+// let num1 = '100';
 
-num = num + 100;
-num1 = num1 + 100;
+// num = num + 100;
+// num1 = num1 + 100;
 
-// console.log(typeof(num));
-// console.log(typeof(num1));
+// // console.log(typeof(num));
+// // console.log(typeof(num1));
 
-//BOOLEAN DATA TYPE IT TAKES TRUE OR FALSE
+// //BOOLEAN DATA TYPE IT TAKES TRUE OR FALSE
  
-let boo = true;
-console.log(boo);
+// let boo = true;
+// console.log(boo);
 
-let arr =['Hallo',1,2,3,'wayne',true,[1,2,3]];
-console.log(typeof(arr))
+// let arr =['Hallo',1,2,3,'wayne',true,[1,2,3]];
+// console.log(typeof(arr))
+
+// const wayne = 'Wayne';
+// // const surName = 'Benhura'
+// // console.log(wayne + " " + surName)
+
+
+// //Assigment  Operators
+// let x = 10 + 5;
+// x += 10;// x = x + 10 = 25
+// x *= 4; // x = x * 4 = 100 
+// x++; //x = x + 1
+// x--; // x = x - 1
+// console.log(x);
+
+// //Comparison Operator
+// let ageSarah = 20;
+// let ageJonas= 21;
+// console.log(ageJonas > ageSarah);
+// console.log(ageSarah <= 18);
+// const isFullage = ageSarah <= 12;
+ 
+// console.log('wayne')
+
+
+
+//IF/ELSE STATEMENTS
+
+const age = 19;
+const isOldEnough = age >=18;
+
+if(isOldEnough){
+    console.log('Wayne can start driving✔')
+}else{
+    const yearsLeft = 18 - age;
+    console.log(`Wayne can't drive. Wait another ${yearsLeft} years.`);
+  }
+
+  const wayne = 30;
+  const hudson = 21;
+  if(wayne <= 18 && hudson){
+    console.log('its okey')
+  }else{
+    console.log('haa mkomana akunyepa')
+  }

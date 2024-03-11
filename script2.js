6406a711f87bc7eb30dbf31f4ea15bb690767e74
HudsonWayne <waynewaynebenhura@gmail.com>
2024-03-11 13:55:20 +0200

    Case loop

--- script2.js
+++ script2.js
@@ -44,20 +44,43 @@
 
 //IF/ELSE STATEMENTS
 
-const age = 19;
-const isOldEnough = age >=18;
+// const age = 19;
+// const isOldEnough = age >=18;
 
-if(isOldEnough){
-    console.log('Wayne can start driving✔')
-}else{
-    const yearsLeft = 18 - age;
-    console.log(`Wayne can't drive. Wait another ${yearsLeft} years.`);
-  }
+// if(isOldEnough){
+//     console.log('Wayne can start driving✔')
+// }else{
+//     const yearsLeft = 18 - age;
+//     console.log(`Wayne can't drive. Wait another ${yearsLeft} years.`);
+//   }
 
-  const wayne = 30;
-  const hudson = 21;
-  if(wayne <= 18 && hudson){
-    console.log('its okey')
-  }else{
-    console.log('haa mkomana akunyepa')
-  }
+//   const wayne = 30;
+//   const hudson = 21;
+//   if(wayne <= 18 && hudson){
+//     console.log('its okey')
+//   }else{
+//     console.log('haa mkomana akunyepa')
+//   }
+
+
+
+//The Switch Statement in JavaScript
+const day = 'Monday'; // Or any day of the week
+
+switch (day) {
+  case 'Monday':
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+    break; // Exit the switch after Monday tasks
+  case 'Tuesday': // Corrected typo
+    console.log('Prepare theory video');
+    break;
+  case 'Wednesday':
+    console.log('Write code examples');
+    break;
+  case 'Thursday':
+    console.log('Write code examples');
+    break;
+  default: // If day doesn't match any case
+    console.log('Relax! It\'s the weekend or an invalid day.');
+}
